e7dcbd50f6469fc588b943287e76b0b7af20b2ca
Patrick Leber <patrick.leber@freenet.de>
2014-10-30 11:10:08 +0100

    Create PoylgonTest

---
+++ test/de/htwg/se/ubongo/geo/PolygonTest.java
@@ -0,0 +1,36 @@
+package de.htwg.se.ubongo.geo;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class PolygonTest {
+
+	@Test
+	public void testConstructor() {
+		//Polygon fp = new Polygon(2);
+		new Polygon(2);
+		new Polygon(5);
+	}
+
+	@Test
+	public void testSetPoint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRotateAround() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMid() {
+		fail("Not yet implemented");
+	}
+
+}
