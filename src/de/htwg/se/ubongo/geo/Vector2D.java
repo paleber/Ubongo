d736df72c725b65aaaef8b960ad1e6b81a585d1d
Patrick Leber <patrick.leber@freenet.de>
2014-11-16 23:16:53 +0100

    add java doc

--- src/de/htwg/se/ubongo/geo/Vector2D.java
+++ src/de/htwg/se/ubongo/geo/Vector2D.java
@@ -10,36 +10,54 @@ public final class Vector2D {
 
     private double x, y;
 
+    /** Default-Constructor initializse values with 0. */
     public Vector2D() {
         set(0, 0);
     }
 
+    /** Value-Constructor.
+     * @param x-Value
+     * @param y-Value */
     public Vector2D(final double x, final double y) {
         set(x, y);
     }
 
+    /** Point-to-Point-Constructor.
+     * @param p from
+     * @param q to */
     public Vector2D(final Point2D p, final Point2D q) {
         set(q.getX() - p.getX(), q.getY() - p.getY());
     }
 
+    /** Copy-Constructor.
+     * @param other other Vector2D */
     public Vector2D(final Vector2D other) {
         x = other.x;
         y = other.y;
     }
 
+    /** Set x- and y-value.
+     * @param x x-value
+     * @param y y-value */
     public void set(final double x, final double y) {
         this.x = x;
         this.y = y;
     }
 
+    /** Get the x-value.
+     * @return x-value */
     public double getX() {
         return x;
     }
 
+    /** Get the y-value.
+     * @return y-value */
     public double getY() {
         return y;
     }
 
+    /** Return the vector as String.
+     * @return Vector as String */
     public String toString() {
         return String.format(Locale.ENGLISH, "<%.3f|%.3f>", x, y);
     }
