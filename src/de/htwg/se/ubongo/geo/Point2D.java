e2ca209ea1b1c14bfda761285b7b22255091a815
Patrick Leber <patrick.leber@freenet.de>
2014-11-16 15:12:04 +0100

    add javadoc

--- src/de/htwg/se/ubongo/geo/Point2D.java
+++ src/de/htwg/se/ubongo/geo/Point2D.java
@@ -39,19 +39,28 @@ public final class Point2D {
         this.y = y;
     }
 
+    /** Get the x-value.
+     * @return x-value */
     public double getX() {
         return x;
     }
 
+    /** Get the y-value.
+     * @return y-value */
     public double getY() {
         return y;
     }
 
+    /** Move along a Vector2D.
+     * @param v movement */
     public void move(Vector2D v) {
         x += v.getX();
         y += v.getY();
     }
 
+    /** Rotate around a other point.
+     * @param angleDeg angle in degree
+     * @param pivot pivot */
     public void rotateAround(final double angleDeg, final Point2D pivot) {
         double angleRad = Math.toRadians(angleDeg);
         double sin = Math.sin(angleRad);
@@ -67,24 +76,34 @@ public final class Point2D {
         y = yn + pivot.y;
     }
 
+    /** Mirror x-Value at y-Axis.
+     * @param yAxis y-Axis */
+    public void mirrorX(int yAxis) {
+        x += (yAxis - x) * FACTOR_DOUBLE;
+    }
+
+    /** Mirror y-Value at x-Axis.
+     * @param xAxis x-Axis */
+    public void mirrorY(int xAxis) {
+        y += (xAxis - y) * FACTOR_DOUBLE;
+    }
+
+    /** Distance to other point.
+     * @param other other point
+     * @return distance */
     public double distanceTo(Point2D other) {
         return Math.sqrt(distanceSquareTo(other));
     }
 
+    /** Squared distance to other point.
+     * @param other other point
+     * @return squared distance */
     public double distanceSquareTo(Point2D other) {
         return (x - other.x) * (x - other.x) + (y - other.y) * (y - other.y);
     }
 
     public String toString() {
         return String.format(Locale.ENGLISH, "(%.3f|%.3f)", x, y);
-    }
-
-    public void mirrorX(int yAxis) {
-        x += (yAxis - x) * FACTOR_DOUBLE;
-    }
-
-    public void mirrorY(int xAxis) {
-        y += (xAxis - y) * FACTOR_DOUBLE;
     }
 
 }
