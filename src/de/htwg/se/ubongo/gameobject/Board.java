7bab7f08e07d4000bc17666974a2db8f3dbab5e7
Patrick Leber <patrick.leber@freenet.de>
2014-11-17 00:14:18 +0100

    add empty methods

--- src/de/htwg/se/ubongo/gameobject/Board.java
+++ src/de/htwg/se/ubongo/gameobject/Board.java
@@ -1,5 +1,6 @@
 package de.htwg.se.ubongo.gameobject;
 
+import java.util.LinkedList;
 import java.util.List;
 
 /** Board.
@@ -7,8 +8,22 @@ import java.util.List;
  * @version 01.11.2014 */
 public final class Board extends BasicBlock {
 
-    public Board(List<Integer> coords) {
+    private final List<Block> blocks = new LinkedList<>();
+
+    public Board(final List<Integer> coords) {
         super(coords);
+    }
+
+    public boolean addBlock(final Block b) {
+        // calc best position
+        // check if free
+        // einrasten wenn frei, return true
+        // ansonsten false
+        return false;
+    }
+
+    public boolean removeBlock(final Block b) {
+        return blocks.remove(b);
     }
 
 }
