f4699362e0bfc5efe79356377fab2c2f994cf8a5
Patrick Leber <patrick.leber@freenet.de>
2014-11-17 17:48:01 +0100

    implement add Block

--- src/de/htwg/se/ubongo/gameobject/Board.java
+++ src/de/htwg/se/ubongo/gameobject/Board.java
@@ -1,5 +1,6 @@
 package de.htwg.se.ubongo.gameobject;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -11,14 +12,47 @@ import de.htwg.se.ubongo.geo.Point2D;
 public final class Board extends AbstractBlock {
 
     private final List<Block> blocks = new LinkedList<>();
-
+    private final List<Point2D> mid = new ArrayList<>();
+    
     public Board(final List<Integer> coords) {
         super(coords);
+        for(int i = 0; i < numPolys(); i++) {
+        	mid.add(getPoly(i).getMid());
+        }
+    }
+    
+    private List<Point2D> calcFreeMids() {
+    	List<Point2D> list = new LinkedList<>(mid);
+    	
+    	for(Block b: blocks) {
+    		for(Point2D p: b.getAnchoredMids()) {
+    			for(Point2D q: list ) {
+    				if(p.distanceSquareTo(q) < 1e-9) {
+    					list.remove(q);
+    					continue;
+    				}
+    			}
+    		}
+    	}
+    	
+    	return list;
     }
 
-    public boolean addBlock(final Block b) {
-    	List<Point2D> mids = b.getAnchoredMids();
-
+    public boolean addBlock(final Block block) {
+    	
+    	// Create
+    	List<Point2D> list = calcFreeMids();
+    	
+    	for(Point2D p: block.getAnchoredMids()) {
+    		for(Point2D q: list) {
+    			
+    		}
+    	}
+    	
+    	
+    	
+    	List<Point2D> blockMids = block.getAnchoredMids();
+    	
         // check if free
         // einrasten wenn frei, return true
         // ansonsten false
