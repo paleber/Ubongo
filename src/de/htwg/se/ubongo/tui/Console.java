28e455ca4ebb0e172c6f6aa9ce47d482f17f6410
Patrick Leber <patrick.leber@freenet.de>
2014-11-18 23:11:08 +0100

    add prototype for graphical Console

---
+++ src/de/htwg/se/ubongo/tui/Console.java
@@ -0,0 +1,24 @@
+package de.htwg.se.ubongo.tui;
+
+/** Static Class Console for TUI Input and Output, only reading and writing
+ * complete lines. */
+public class Console {
+
+    /* Private Constructor, Utility Class. */
+    private Console() {
+    }
+
+    /** Write line in Console.
+     * @param line line */
+    public static void write(String line) {
+        // TODO
+    }
+
+    /** Read the next Line. Return null if not available.
+     * @return next line if available, otherise null */
+    public static String read() {
+        // TODO
+        return null;
+    }
+
+}
